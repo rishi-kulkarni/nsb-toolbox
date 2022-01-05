4e821d6ad3c637c71c0bfc6c3feb013f4e4cf45b
Rishi Kulkarni <rishi@kulkarni.science>
2022-01-04 23:04:18 -0500

    lexer impl

--- nsbtoolbox/parser.py
+++ nsbtoolbox/parser.py
@@ -1,25 +1,25 @@
 import re
-
+from dataclasses import dataclass
 from .sciencebowlquestion import QuestionType, ScienceBowlQuestion, Subject, TossUpBonus
 
 TUB = ("TOSS-UP", "BONUS", "VISUAL BONUS")
 
 SUBJECT_ALIASES = (
-    "Life Science",
+    "LIFE",
     "LS",
-    "Biology",
+    "BIOLOGY",
     "B",
-    "Physical Science",
+    "PHYSICAL",
     "PS",
-    "Chemistry",
+    "CHEMISTRY",
     "C",
-    "Physics",
+    "PHYSICS",
     "P",
-    "Math",
+    "MATH",
     "M",
-    "Earth and Space",
+    "EARTH",
     "ES",
-    "Energy",
+    "ENERGY",
     "EN",
 )
 
@@ -28,8 +28,82 @@ QUESTION_TYPES = ("Multiple Choice", "MC", "Short Answer", "SA")
 MC_CHOICES = ("W)", "X)", "Y)", "Z)")
 
 
+@dataclass
+class Token:
+    id: str
+    type: str
+
+
+def lexer(inputstream: list):
+
+    iterator = enumerate(inputstream)
+
+    for idx, word in iterator:
+
+        next_word = peek(inputstream, idx).upper()
+        current_word = word.upper()
+
+        if current_word == "ROUND":
+            if next_word.isdigit():
+                yield Token(int(next_word), "ROUNDNUM")
+                next(iterator, None)
+            else:
+                yield Token(word, "WORD")
+
+        elif current_word in SUBJECT_ALIASES:
+
+            if current_word in ("LIFE", "PHYSICAL", "EARTH"):
+                if next_word == "SCIENCE":
+                    yield Token(word + " Science", "SUBJECT")
+                    next(iterator, None)
+                elif (
+                    next_word.upper() == "AND"
+                    and peek(inputstream, idx, distance=2).upper() == "SPACE"
+                ):
+                    yield Token("Earth and Space", "SUBJECT")
+                    next(iterator, None)
+                    next(iterator, None)
+            else:
+                yield Token(word, "SUBJECT")
+
+        elif current_word == "MULTIPLE" and next_word == "CHOICE":
+            yield Token("Multiple Choice", "QTYPE")
+            next(iterator, None)
+        elif current_word == "SHORT" and next_word == "ANSWER":
+            yield Token("Short Answer", "QTYPE")
+            next(iterator, None)
+        elif current_word in ("MC", "SA"):
+            yield Token(word, "QTYPE")
+
+        elif current_word in TUB:
+            if next_word == "BONUS":
+                yield Token("VISUAL BONUS", "TUB")
+                next(iterator)
+            else:
+                yield Token(word, "TUB")
+
+        elif ")" in current_word:
+            if current_word.replace(")", "").isdigit():
+                yield Token(word, "NUMID")
+            elif current_word in MC_CHOICES:
+                yield Token(word, "WXYZ")
+
+        elif current_word == "ANSWER:":
+            yield Token(word, "ANSWER")
+
+        else:
+            yield Token(word, "WORD")
+
+
+def peek(inputstream: list, idx: int, distance=1):
+    if idx + distance < len(inputstream):
+        return inputstream[idx + distance]
+    else:
+        return ""
+
+
 def question_parser(input: str):
-    
+
     question_list = []
     pos = 0
 
@@ -62,5 +136,3 @@ def question_parser(input: str):
 
         if constructor_dict["question_type"] is QuestionType.MULTIPLE_CHOICE:
             pass
-
-
