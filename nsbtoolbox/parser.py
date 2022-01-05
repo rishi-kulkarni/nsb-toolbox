58ed7238cd9e6b4a730327449211d1433a7c1954
Rishi Kulkarni <rishi@kulkarni.science>
2022-01-04 20:59:00 -0500

    separated IO from lexer

--- nsbtoolbox/parser.py
+++ nsbtoolbox/parser.py
@@ -1,8 +1,4 @@
-import argparse
-import os
 import re
-
-import docx2txt
 
 from .sciencebowlquestion import QuestionType, ScienceBowlQuestion, Subject, TossUpBonus
 
@@ -68,42 +64,3 @@ def question_parser(input: str):
             pass
 
 
-def validate_path(path_string: str) -> str:
-    """Validates that incoming path exists.
-
-    Parameters
-    ----------
-    path_string : str
-
-    Returns
-    -------
-    path_string
-
-    Raises
-    ------
-    FileNotFoundError
-
-    """
-    if os.path.exists(path_string):
-        return path_string
-    else:
-        raise FileNotFoundError(f"No such file: {path_string}")
-
-
-if __name__ == "__main__":
-
-    argparser = argparse.ArgumentParser(
-        description="Parser for Science Bowl .docx files."
-    )
-    argparser.add_argument(
-        "path",
-        metavar="path",
-        type=str,
-        help="path to the Science Bowl docx file",
-    )
-
-    args = argparser.parse_args()
-
-    path_to_data = validate_path(args.path)
-
-    raw_text = docx2txt.process(path_to_data)
