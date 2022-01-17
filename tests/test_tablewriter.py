fe012f5d8492e7ffa28c7a42422b134fd552081e
Rishi Kulkarni <rishi@kulkarni.science>
2022-01-16 23:07:46 -0500

    add tests for subject and tub cell formatters

--- tests/test_tablewriter.py
+++ tests/test_tablewriter.py
@@ -1,10 +1,10 @@
 import unittest
 from pathlib import Path
 
-from nsbtoolbox import tablewriter
-
-from docx.shared import Pt
 from docx import Document
+from docx.enum.text import WD_COLOR_INDEX
+from docx.shared import Pt
+from nsbtoolbox import tablewriter
 
 data_dir = Path(__file__).parent / "test_data"
 
@@ -114,3 +114,79 @@ class TestPreprocessCell(unittest.TestCase):
 
     def test_save(self):
         self.test_data.save(self.temp_data)
+
+
+class TestFormatTUBCell(unittest.TestCase):
+
+    test_data = Document(data_dir / "test_TUB.docx")
+
+    def _check(self, formatted_cell, expected_text):
+        self.assertEqual(
+            tablewriter.format_tub_cell(formatted_cell).text, expected_text
+        )
+
+        self.assertEqual(len(formatted_cell.paragraphs), 1)
+
+        cell_runs = formatted_cell.paragraphs[0].runs
+        self.assertEqual(len(cell_runs), 1)
+        self.assertIsNone(cell_runs[0].font.italic)
+        self.assertIsNone(cell_runs[0].font.bold)
+
+    def test_TUB(self):
+        TUB = ("TOSS-UP", "BONUS", "VISUAL BONUS")
+        test_rows = self.test_data.tables[0].rows[:3]
+
+        for row, tub_expected in zip(test_rows, TUB):
+            for cell in row.cells:
+                self._check(tablewriter.format_tub_cell(cell), tub_expected)
+
+    def test_errors(self):
+        error_row = self.test_data.tables[0].rows[3]
+        for cell in error_row.cells:
+            prior_text = cell.text
+            test_run = tablewriter.format_tub_cell(cell).paragraphs[0].runs[0]
+            after_text = cell.text
+            self.assertEqual(prior_text, after_text)
+            self.assertEqual(test_run.font.highlight_color, WD_COLOR_INDEX.RED)
+
+
+class TestFormatSubject(unittest.TestCase):
+
+    test_data = Document(data_dir / "test_subject.docx")
+
+    def _check(self, formatted_cell, expected_text):
+        self.assertEqual(
+            tablewriter.format_tub_cell(formatted_cell).text, expected_text
+        )
+
+        self.assertEqual(len(formatted_cell.paragraphs), 1)
+
+        cell_runs = formatted_cell.paragraphs[0].runs
+        self.assertEqual(len(cell_runs), 1)
+        self.assertIsNone(cell_runs[0].font.italic)
+        self.assertIsNone(cell_runs[0].font.bold)
+
+    def test_subject(self):
+        SUBJECTS = (
+            "Biology",
+            "Chemistry",
+            "Physics",
+            "Earth and Space",
+            "Math",
+            "Energy",
+        )
+
+        test_rows = self.test_data.tables[0].rows[:6]
+
+        for row, subject in zip(test_rows, SUBJECTS):
+            for cell in row.cells:
+                self._check(tablewriter.format_subject_cell(cell), subject)
+
+    def test_errors(self):
+        error_row = self.test_data.tables[0].rows[6]
+        for cell in error_row.cells:
+            prior_text = cell.text
+            test_run = tablewriter.format_tub_cell(cell).paragraphs[0].runs[0]
+            after_text = cell.text
+            self.assertEqual(prior_text, after_text)
+            self.assertEqual(test_run.font.highlight_color, WD_COLOR_INDEX.RED)
